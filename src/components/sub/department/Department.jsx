809c5dfbc572f832c0074f348ccf19b0d389d427
yhy7805 <yhy7805@gmail.com>
2023-10-11 13:29:41 +0900

    프론트엔드 개발 기본지식

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,14 +1,16 @@
+
+//Layout 컴포넌트를 불러옴 ../../ 구문은 부모 디렉터리로 이동하기 위해 사용됨
 import Layout from '../../common/layout/Layout';
+//2.데이터가 다 받아지고 useState로 state에 해당 담아줌
 import { useEffect, useState } from 'react';
 import './Department.scss';
 const path = process.env.PUBLIC_URL;
-//1.useEffect로 컴포넌트 마운트되자마자 fetch외부데이터 가져옴
-//2.데이터가 다 받아지면 useState로 state에 해당 담아줌
-//3.return문 안족에 state값을 map으로 반복돌면서 JSX출력
 
 export default function Department() {
 	const [Department, setDepartment] = useState([]);
 
+	//UseEffect 컴포넌트 마운트 되자마자 fetch로 외부데이터를 가져온다
+	//마운트 : 컴포넌트가 화면에 나타나는 것
 	useEffect(() => {
 		fetch(`${path}/DB/department.json`)
 			.then((data) => data.json())
@@ -20,6 +22,7 @@ export default function Department() {
 	return (
 		<Layout title={'Department'}>
 			<div className='memberBox'>
+				{/* return 문 안쪽에 state값 map으로 반복 돌며 JSX를 출력한다 */}
 				{Department.map((member, idx) => {
 					return (
 						<article key={idx}>
