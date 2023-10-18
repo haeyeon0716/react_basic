438b2ce19ab725e083b44186f698fb1b5383c25f
yhy7805 <yhy7805@gmail.com>
2023-10-18 10:13:14 +0900

    메인페이지 flickr 전역 데이터 출력

--- src/components/main/info/Info.jsx
+++ src/components/main/info/Info.jsx
@@ -1,16 +1,25 @@
+import { useSelector } from 'react-redux';
 import './Info.scss';
 
 function Info() {
+	const { data } = useSelector((store) => store.flickr);
 	return (
 		<section className='info'>
-			<div className="wrap">
-                <article>1</article>
-                <article>2</article>
-                <article>3</article>
-                <article>4</article>
-                <article>5</article>
-                <article>6</article>
-            </div>
+			{data.map((pic, idx) => {
+				return (
+					<article key={idx}>
+						<h2>{pic.title}</h2>
+					</article>
+				);
+			})}
+			<div className='wrap'>
+				<article>1</article>
+				<article>2</article>
+				<article>3</article>
+				<article>4</article>
+				<article>5</article>
+				<article>6</article>
+			</div>
 		</section>
 	);
 }
