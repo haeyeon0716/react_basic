c1d3da4f6b3b72b0a287cb98d4fb2baa5ae0a846
yhy7805 <yhy7805@gmail.com>
2023-10-19 16:23:17 +0900

    Visual영역에서 스와이퍼 버튼 클릭시 유튜브 상세페이지 넘김

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { useState } from 'react';
 import 'swiper/css';
+import { Link } from 'react-router-dom';
 
 function Visual() {
 	const { data } = useSelector((store) => store.youtube);
@@ -17,8 +18,10 @@ function Visual() {
 						return (
 							<li key={idx} className={idx === Index ? 'on' : ''}>
 								<h3>{tit.snippet.title}</h3>
-								<p>{tit.snippet.description.substr(0, 50) + '...'}</p>
-								<button>View Deatil</button>
+								<p>{tit.snippet.description.substr(0, 300) + '...'}</p>
+								<button>
+									<Link to={`/detail/${tit.id}`}>View Deatil</Link>
+								</button>
 							</li>
 						);
 					})}
