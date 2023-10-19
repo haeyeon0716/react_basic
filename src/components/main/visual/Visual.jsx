c961b8cc517e809ebb78dacab4e7dab4d51a5fdf
yhy7805 <yhy7805@gmail.com>
2023-10-19 13:30:04 +0900

    버튼 활성화 모션

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,23 +1,45 @@
 import './Visual.scss';
 import { useSelector } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useState } from 'react';
 import 'swiper/css';
 
 function Visual() {
 	const { data } = useSelector((store) => store.youtube);
+	const [Index, setIndex] = useState(0);
+	console.log(data);
 
 	return (
 		<section className='visual'>
-			<Swiper slidesPerView={3} spaceBetween={50}>
+			<div className='titBox'>
+				<ul>
+					{data.map((tit, idx) => {
+						if (idx >= 5) return null;
+						return (
+							<li key={idx} className={idx === Index ? 'on' : ''}>
+								<h3>{tit.snippet.title}</h3>
+
+								<button>View Detail</button>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+			<Swiper
+				slidesPerView={3}
+				spaceBetween={50}
+				loop={true}
+				centeredSlides={true}
+				//swiper loop 기능을 적용하는 순간 실제 연결되어 있는 패널갯수보다 동적으로 패널이 생성되면서 일반적인 방법으로는 활성화패널의 순서값을 구할 수 없기 때문에 아래와 같은 방법으로 순서값을 구함
+				onSlideChange={(el) => setIndex(el.realIndex)}
+			>
 				{data.map((vid, idx) => {
 					if (idx >= 5) return null;
 					return (
 						<SwiperSlide key={idx}>
-							<div className='inner'>
-								<div className="pic">
+							<div className='pic'>
 								<img src={vid.snippet.thumbnails.maxres.url} alt={vid.title} />
-								</div>
-								<h2>{vid.snippet.title}</h2>
+								<img src={vid.snippet.thumbnails.maxres.url} alt={vid.title} />
 							</div>
 						</SwiperSlide>
 					);
